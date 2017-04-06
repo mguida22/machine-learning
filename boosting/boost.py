65b069b9e4cc206f2c37ef53e71284bf8ba6ac11
mguida22 <michael.guida@colorado.edu>
2017-04-06 12:45:31 -0600

    pep8 boost

--- boosting/boost.py
+++ boosting/boost.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 
 np.random.seed(1234)
 
+
 class FoursAndNines:
     """
     Class to store MNIST data
@@ -16,7 +17,8 @@ class FoursAndNines:
 
     def __init__(self, location):
 
-        import cPickle, gzip
+        import cPickle
+        import gzip
 
         # Load the dataset
         f = gzip.open(location, 'rb')
@@ -25,29 +27,37 @@ class FoursAndNines:
         train_set, valid_set, test_set = cPickle.load(f)
 
         # Extract only 4's and 9's for training set
-        self.x_train = train_set[0][np.where(np.logical_or( train_set[1]==4, train_set[1] == 9))[0],:]
-        self.y_train = train_set[1][np.where(np.logical_or( train_set[1]==4, train_set[1] == 9))[0]]
+        self.x_train = train_set[0][np.where(np.logical_or(
+            train_set[1] == 4, train_set[1] == 9))[0], :]
+        self.y_train = train_set[1][
+            np.where(np.logical_or(train_set[1] == 4, train_set[1] == 9))[0]]
         self.y_train = np.array([1 if y == 9 else -1 for y in self.y_train])
 
         # Shuffle the training data
         shuff = np.arange(self.x_train.shape[0])
         np.random.shuffle(shuff)
-        self.x_train = self.x_train[shuff,:]
+        self.x_train = self.x_train[shuff, :]
         self.y_train = self.y_train[shuff]
 
         # Extract only 4's and 9's for validation set
-        self.x_valid = valid_set[0][np.where(np.logical_or( valid_set[1]==4, valid_set[1] == 9))[0],:]
-        self.y_valid = valid_set[1][np.where(np.logical_or( valid_set[1]==4, valid_set[1] == 9))[0]]
+        self.x_valid = valid_set[0][np.where(np.logical_or(
+            valid_set[1] == 4, valid_set[1] == 9))[0], :]
+        self.y_valid = valid_set[1][
+            np.where(np.logical_or(valid_set[1] == 4, valid_set[1] == 9))[0]]
         self.y_valid = np.array([1 if y == 9 else -1 for y in self.y_valid])
 
         # Extract only 4's and 9's for test set
-        self.x_test  = test_set[0][np.where(np.logical_or( test_set[1]==4, test_set[1] == 9))[0],:]
-        self.y_test  = test_set[1][np.where(np.logical_or( test_set[1]==4, test_set[1] == 9))[0]]
+        self.x_test = test_set[0][np.where(np.logical_or(
+            test_set[1] == 4, test_set[1] == 9))[0], :]
+        self.y_test = test_set[1][
+            np.where(np.logical_or(test_set[1] == 4, test_set[1] == 9))[0]]
         self.y_test = np.array([1 if y == 9 else -1 for y in self.y_test])
 
         f.close()
 
+
 class AdaBoost:
+
     def __init__(self, n_learners=20, base=DecisionTreeClassifier(max_depth=1)):
         """
         Create a new adaboost classifier.
@@ -106,8 +116,7 @@ class AdaBoost:
             self.alpha[k] = 0.5 * math.log((1 - err) / err)
 
             # update weight
-            w =w * np.exp(-1 * self.alpha[k] * predictions * y_train)
-
+            w = w * np.exp(-1 * self.alpha[k] * predictions * y_train)
 
     def predict(self, X):
         """
@@ -145,7 +154,7 @@ class AdaBoost:
             if y[i] == predictions[i]:
                 score += 1
 
-        return score/len(y)
+        return score / len(y)
 
     def staged_score(self, X, y):
         """
@@ -165,7 +174,7 @@ class AdaBoost:
 
         for n in range(self.n_learners):
             sum_a_h_x = 0
-            for k in range(n+1):
+            for k in range(n + 1):
                 sum_a_h_x += self.alpha[k] * (self.learners[k].predict(X))
 
             predictions = np.sign(sum_a_h_x)
@@ -175,7 +184,7 @@ class AdaBoost:
                 if y[i] == predictions[i]:
                     score += 1
 
-            scores[n] = score/len(y)
+            scores[n] = score / len(y)
 
         return scores
 
@@ -184,7 +193,7 @@ def mnist_digit_show(flatimage, outname=None):
 
 	import matplotlib.pyplot as plt
 
-	image = np.reshape(flatimage, (-1,28))
+	image = np.reshape(flatimage, (-1, 28))
 
 	plt.matshow(image, cmap=plt.cm.binary)
 	plt.xticks([])
@@ -196,15 +205,17 @@ def mnist_digit_show(flatimage, outname=None):
 
 if __name__ == "__main__":
 
-	parser = argparse.ArgumentParser(description='AdaBoost classifier options')
-	parser.add_argument('--limit', type=int, default=-1,
+    parser = argparse.ArgumentParser(description='AdaBoost classifier options')
+    parser.add_argument('--limit', type=int, default=-1,
                         help="Restrict training to this many examples")
-	parser.add_argument('--n_learners', type=int, default=50,
+    parser.add_argument('--n_learners', type=int, default=50,
                         help="Number of weak learners to use in boosting")
-	args = parser.parse_args()
+    args = parser.parse_args()
 
-	data = FoursAndNines("../data/mnist.pkl.gz")
+    data = FoursAndNines("../data/mnist.pkl.gz")
 
     # An example of how your classifier might be called
-	clf = AdaBoost(n_learners=50, base=DecisionTreeClassifier(max_depth=1, criterion="entropy"))
-	clf.fit(data.x_train, data.y_train)
+    clf = AdaBoost(n_learners=50, base=DecisionTreeClassifier(max_depth=1, criterion="entropy"))
+    clf.fit(data.x_train, data.y_train)
+
+    clf.score(data.y_test)
